12431dd983177148095c9dc6c31d732a5948f1a5
Joachim Thorsen Larsen <joacla14@uia.no>
2022-05-22 10:44:30 +0200

    moved processes

--- MA223_exam_examples/bernoulli_poisson_gaussisk_processes.R
+++ MA223_exam_examples/bernoulli_poisson_gaussisk_processes.R
@@ -1,64 +1,6 @@
-
-# Misc -------------------------------------------------------------------
-
-# Dummydata show how to use examples
-data_x = c(0); data_y = c(0); n = 1
-
-# List of OK colors to use in case more than a few are needed
-colors = c("#400043","#B03060","#0005a9","#8fffb4","#a94bfc","#ffd56e","#938aff","#005524","#d40032","#01bca1","#b3005f","#4dd4ff","#713700","#ffb6f2","#00333c","#ffb39b")
-
-
-# Libraries
-library("datasets")  # For datasets
-library("readxl")    # Lese excel filer
-library(metRology)   # t-fordeling, beta binom, negativ beta binom
-library(extraDistr)  # gamma-gamma-fordeling
-library(matlib)
-library(car)
-
-# 1 Weighted measures / vekttall ------------------------------------------
-
-test = dbbinom(1, 2, 3, 4)
-
-# * 1.1 Varians -----------------------------------------------------------
-
-# SSx
-SSx = sum(data^2) - (sum(data)^2 / length(data))
-
-# SSxy
-SSxy = sum(data * data_y) - (sum(data) * sum(data_y) / length(data))
-
-# Populasjons varians
-sigma_x2 = SSx / n       #  sigma^2_x
-
-# Populasjons avvik
-sigma_x = sqrt(pop_var)  #  sigma_x
-
-# Utvalgs varians
-s_x2 = SSx / (n - 1)     #  s^2_x
-
-# Utvalgs avvik
-s_x = sqrt(utv_var)      # s_x
-
-# Populasjons kovarians for x, y
-sigma_xy = SSxy / n
-
-# Utvalgs kovarians
-s_xy = SSxy / (n - 1)
-
-# Korrellasjon
-rho_xy = sima_xy / sigma_x * sigma_y  #  sigma_y = sigma_x for y verdier
-
-r_xy = s_xy / s_x * s_y               #  s_y = s_x for y verdier
-  
-
-
-
 # Processes -------------------------------------------------------------
-
-# * Bernoulli Process -- Eksamen desember 2021 --------------------------
-
-
+# Bernoulli Process -- Eksamen desember 2021 --------------------------
+{
 # ** a) Ligninggenerator ----
 # Nyberg har laget en generator for tredjegradsligninger.
 # Sannsynligheten for at Sebastian klarer å løse en tilfeldig generert ligning derfra
@@ -79,7 +21,7 @@ to_eller_flere = 1 - pbinom(1, k, p)
 # *** iii. Ber om nye ligninger helt til han klarer å løse 2 av dem ----
 # Y ~nb(2, 3/8)(x) Y = dbnbinom(, 2, p) 
 
-  
+
 # ** b) Avløpsvann ----
 # Du har fått i sommerjobb å passe på rensing av avløpsvann i en storby. I dag
 # ser du spesielt etter gummiprodukter, og erfaringen fra tidligere år tilsier at
@@ -106,10 +48,10 @@ abline(v = expectedVal, col ="maroon")
 abline(v = medianVann, col="blue")
 abline(v = modeVann, col="purple")
 
-
 # *** iv. Sannsynlighetsfordelingen for antall gummiprodukter i 100 kubikkmeter med  ----
 # pois ~(0.017, 100)(x)
 
-# * Poisson Process --  ----------------------------
 
-# * Gaussian Process --  ---------------------------
+}
+# Poisson Process
+# **
---
+++ MA223_exam_examples/ma223_varians_and_useful_snippets.R
@@ -0,0 +1,70 @@
+
+# Misc -------------------------------------------------------------------
+
+# Libraries
+library("datasets")  # For datasets
+library("readxl")    # Lese excel filer
+library(metRology)   # t-fordeling, beta binom, negativ beta binom
+library(extraDistr)  # gamma-gamma-fordeling
+library(matlib)
+library(car)
+
+# Dummydata show how to use examples
+data_x = c(0); data_y = c(0); n = 1
+
+# List of OK colors to use in case more than a few are needed
+colors = c("#400043","#B03060","#0005a9","#8fffb4","#a94bfc","#ffd56e","#938aff","#005524","#d40032","#01bca1","#b3005f","#4dd4ff","#713700","#ffb6f2","#00333c","#ffb39b")
+
+
+# Highlight interval under graph
+yVals=dbetapr(xVals,2,6,19.3)
+plot(xVals,yVals,type="l",col="maroon",lwd=2)
+lines(xVals,yVals*(xVals<5),type="h",col="yellow")
+
+# Draw the graph again as a line to fix outline
+lines(xVals,yVals,type="l",col="maroon",lwd=2)
+
+# Adding legend graph to show what each line is. https://r-coder.com/add-legend-r/
+legend(x = "topright", 
+       title = "Title",
+       legend = c("graph", "interval"), 
+       # 1 = full, 2 = lines, 3 = dotted 
+       lty = c(1, 1), 
+       col = c("maroon", "yellow"), 
+       lwd = 4,
+       # Change legend scale
+       cex = 2)
+
+
+
+# 1.1 Varians -----------------------------------------------------------
+
+# SSx
+SSx = sum(data^2) - (sum(data)^2 / length(data))
+
+# SSxy
+SSxy = sum(data * data_y) - (sum(data) * sum(data_y) / length(data))
+
+# Populasjons varians
+sigma_x2 = SSx / n       #  sigma^2_x
+
+# Populasjons avvik
+sigma_x = sqrt(pop_var)  #  sigma_x
+
+# Utvalgs varians
+s_x2 = SSx / (n - 1)     #  s^2_x
+
+# Utvalgs avvik
+s_x = sqrt(utv_var)      # s_x
+
+# Populasjons kovarians for x, y
+sigma_xy = SSxy / n
+
+# Utvalgs kovarians
+s_xy = SSxy / (n - 1)
+
+# Korrellasjon
+rho_xy = sima_xy / sigma_x * sigma_y  #  sigma_y = sigma_x for y verdier
+
+r_xy = s_xy / s_x * s_y               #  s_y = s_x for y verdier
+
